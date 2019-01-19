8232ae177b4d882fd7b05cf2e37fea28be467d67
tfreeman1 <tfreeman@pletter.com>
2019-01-19 15:13:05 -0800

    [master] - fixing docker compose and unit tests

--- tests/unitTests.js
+++ tests/unitTests.js
@@ -2,9 +2,9 @@ const test = require('tape')
 const request = require('supertest')
 const express = require('express')
 
-const Document = require('../models/Document')
+const Snack = require('../models/Snack')
 const app = require('../index')
-let documentId
+let secret
 
 before(done => {
   app.on( 'APP_STARTED', () => {
@@ -14,60 +14,38 @@ before(done => {
 
 describe('API Integration Test', () => {
   it('Runs all tests', done => {
-    test('/api/documents/new', assert => {
+    test('/api/snacks/new', assert => {
       request(app)
-        .post('/api/documents/new')
-        .send(new Document('test title', 'test user', 'test body'))
+        .post('/api/snacks/new')
+        .send(new Snack('12345', 'testsnack'))
         .expect(200)
         .end((err, res) => {
           if (err) return assert.fail(JSON.stringify(res))
-          assert.pass('Created a new document successfully, test passed!')
+          assert.pass('Created a new Snack successfully, test passed!')
           assert.end()
         })
     })
 
-    test('/api/documents/all', assert => {
+
+    test('/api/snacks/:secret', assert => {
       request(app)
-        .get('/api/documents/all')
+        .get(`/api/snacks/${secret}`)
         .expect(200)
         .end((err, res) => {
           if (err) return assert.fail(JSON.stringify(res))
-          documentId = res.body[0]._id
-          assert.pass('Got all documents successfully, test passed!')
+          assert.pass('Got a specific Snack successfully, test passed!')
           assert.end()
         })
     })
 
-    test('/api/documents/:id', assert => {
-      request(app)
-        .get(`/api/documents/${documentId}`)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return assert.fail(JSON.stringify(res))
-          assert.pass('Got a specific document successfully, test passed!')
-          assert.end()
-        })
-    })
 
-    test('/api/documents/edit/:id', assert => {
+    test('/api/snacks/delete/:secret', assert => {
       request(app)
-        .patch(`/api/documents/edit/${documentId}`)
-        .send(new Document('test title edit', 'test user edit', 'test body edit'))
+        .delete(`/api/snacks/delete/${secret}`)
         .expect(200)
         .end((err, res) => {
           if (err) return assert.fail(JSON.stringify(res))
-          assert.pass('Edited a document successfully, test passed!')
-          assert.end()
-        })
-    })
-
-    test('/api/documents/delete/:id', assert => {
-      request(app)
-        .delete(`/api/documents/delete/${documentId}`)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return assert.fail(JSON.stringify(res))
-          assert.pass('Deleted a specific document successfully, test passed!')
+          assert.pass('Deleted a specific Snack successfully, test passed!')
           assert.end()
           done()
         })
