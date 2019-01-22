b7caf12e27056511e3e6f7dded2b4bf39965d1c0
tfreeman1 <tfreeman@pletter.com>
2019-01-21 22:56:48 -0800

    [master] - updating unit tests

--- test/unitTests.js
+++ test/unitTests.js
@@ -17,19 +17,19 @@ describe('API Integration Test', () => {
   it('Runs all tests', done => {
     test('/api/snacks/new', assert => {
       request(app)
-        .set('client_secret', ${secret})
-        .get('/api/snacks/new/${value}')
+        .set('client_secret', 1234)
+        .get('/api/snacks/new/teststring')
         .expect(200)
         .end((err, res) => {
           if (err) return assert.fail(JSON.stringify(res))
           assert.pass('Created a new Snack successfully, test passed!')
-          assert.end()
+          assert.end()å
         })
     })
 
     test('/api/snacks/find/', assert => {
       request(app)
-        .set('client_secret', ${secret})
+        .set('client_secret', 1234)
         .get('/api/snacks/find/')
         .expect(200)
         .end((err, res) => {
@@ -41,8 +41,20 @@ describe('API Integration Test', () => {
 
     test('/api/snacks/findall/', assert => {
       request(app)
-        .set('client_secret', ${secret})
+        .set('client_secret', 1187)
         .get('/api/snacks/findall/')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return assert.fail(JSON.stringify(res))
+          assert.pass('Got a specific Snack successfully, test passed!')
+          assert.end()
+        })
+    })
+
+    test('/api/snacks/delete/', assert => {
+      request(app)
+        .set('client_secret', 1234)
+        .get('/api/snacks/delete/')
         .expect(200)
         .end((err, res) => {
           if (err) return assert.fail(JSON.stringify(res))
